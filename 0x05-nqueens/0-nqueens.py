364c5c94d30cf6e444796a13329d745c664ccaa8
Odo-Peter <odopeter33@gmail.com>
2024-01-20 03:17:14 -0800

    solved the tasks

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Module for 0x0C. N Queens.
+ALX Africa
+Specializations - Interview Preparation ― Algorithms
+"""
+from sys import argv, exit
+
+
+def solveNQueens(n):
+    """Program that solves the N queens problem"""
+    res = []
+    queens = [-1] * n
+
+    def dfs(index):
+        """Recursively resolves the N queens problem"""
+        if index == len(queens):
+            res.append(queens[:])
+            return
+        for i in range(len(queens)):
+            queens[index] = i
+            if valid(index):
+                dfs(index + 1)
+
+    def valid(n):
+        """Method that checks if a position in the board is valid"""
+        for i in range(n):
+            if abs(queens[i] - queens[n]) == n - i:
+                return False
+            if queens[i] == queens[n]:
+                return False
+        return True
+
+    def make_all_boards(res):
+        """Method that builts the List that be returned"""
+        actual_boards = []
+        for queens in res:
+            board = []
+            for row, col in enumerate(queens):
+                board.append([row, col])
+            actual_boards.append(board)
+        return actual_boards
+
+    dfs(0)
+    return make_all_boards(res)
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+    else:
+        result = solveNQueens(n)
+        for row in result:
+            print(row)
